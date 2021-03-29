1d14004b16e0b8a78b9f775f28110defc2992197
Radu Lazar <radu.lazar@live.com>
2021-03-29 15:11:13 +0300

    changed

--- deploy.PS.ps1
+++ deploy.PS.ps1
@@ -2,34 +2,32 @@ Connect-AzAccount
 Get-AzContext
 Get-AzSubscription
 
-## deploy to AzSubscription
-#   New-AzSubscriptionDeployment -Location <location> -TemplateFile <path-to-template-or-bicep>
-## deploy to Resource Group
-#   New-AzResourceGroupDeployment -ResourceGroupName <resource-group-name> -TemplateFile <path-to-template-or-bicep>
-
-
+# where endpoints are created. Each VM receive a NAT-ed IP in this subnet  
 $endpointVirtualNetwork = "OnPremConnect-vNet"
 $endpointVirtualNetworkSubnet = "192.168.1.0_24"
-
 $subnetId = (Get-AzVirtualNetwork -name $endpointVirtualNetwork | Get-AzVirtualNetworkSubnetConfig -name $endpointVirtualNetworkSubnet).Id
 
+$imageGallery = "BoxImages" 
+$location = "West Europe"
+$vmfile = ".\vmlist.csv"
 
 # BUILD LAB
 
 # create Resource Group and guid to identify the lab. output: rgName and guid
-$rgDeployResult = New-AzSubscriptionDeployment -Location 'West Europe' -TemplateFile .\arm\rg.json
+$rgDeployResult = New-AzSubscriptionDeployment -Location $location -TemplateFile .\arm\rg.json
 
 $rg = $rgDeployResult.Outputs.rgname.Value
 $guid = $rgDeployResult.Outputs.guid.Value
+
 # Create Virtual Network
 New-AzResourceGroupDeployment -ResourceGroupName $rg -guid $guid -TemplateFile .\arm\virtualnetwork.json
 
-$VMs = Import-Csv .\deploy.PS.parameters.csv -Delimiter ","
-## for each VM:
+$VMs = Import-Csv $vmfile -Delimiter ","
+
 foreach ($vm in $VMs) {
 
     # Create VM
-    New-AzResourceGroupDeployment -ResourceGroupName $rg -guid $guid -vmName $vm.Name -vmIndex $vm.Index -TemplateFile .\arm\vm.json
+    New-AzResourceGroupDeployment -ResourceGroupName $rg -guid $guid -vmName $vm.Name -vmIndex $vm.Index -imageGallery $imageGallery -TemplateFile .\arm\vm.json
 
     # Create Private Endpoint in existing vnet. 
     # Note: Private Endpoint Network Policies must be disabled for that vnet. You can create a Private Endpoint in the portal to auto-disable the policy
