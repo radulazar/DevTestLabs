a128294b0f46e473b2a5a4fb550209cc1998f0a0
Radu Lazar <radu.lazar@live.com>
2021-03-31 12:43:12 +0300

    update register dns records script

--- scripts/ReadAndRegisterDnsRecords.ps1
+++ scripts/ReadAndRegisterDnsRecords.ps1
@@ -2,6 +2,11 @@ param(
     [string]$guid,
     [string]$DnsZoneName = 'test.zone'
 )
+# DNS zone to be created inside $DnsZoneName
+$guidDNSZoneName = $guid + "." + $ZoneName
+# for stand-alone DNS server
+$guidDNSZoneFile = $boxZone + ".dns"
+
 
 # Auth to Azure
 #Install-Module Az
@@ -10,14 +15,11 @@ param(
 
 
 function read-Azure-records{
-
 param([string] $guid)
 
 $pendp_Resources = Get-AzResource -Tag @{ "env" = $guid} -ResourceType "Microsoft.Network/privateEndpoints" 
-
 # array of hashtables with vm & IP information to be added to DNS, one hastable object per DNS record
 $arrayDnsRecords = @()
-
 foreach ($pendp_Resource in $pendp_Resources){
     $pendp_Nic = (Get-AzPrivateEndpoint -Name $pendp_Resource.Name).NetworkInterfaces[0]
     $nicObj = Get-AzNetworkInterface -ResourceId $pendp_Nic.Id
@@ -26,13 +28,14 @@ foreach ($pendp_Resource in $pendp_Resources){
 return $arrayDnsRecords
 }
 
-
+## Get info from Azure and register in onprem DNS
 $DnsRecords = read-Azure-records($guid)
 
-foreach ($DnsRecord in $DnsRecords){
-    $ARecord = $DnsRecord.vm + "." + $guid
-    Add-DnsServerResourceRecordA -ZoneName $ZoneName -Name $ARecord -IPv4Address $DnsRecord.IP -computername localhost -CreatePtr
-    #Get-DnsServerResourceRecord -ZoneName $ZoneName | fl hostname
-}
+# create DNS zone for the environment
+Add-DnsServerPrimaryZone -Name $guidDNSZoneName -zonefile $guidDNSZoneFile
 
-Get-DnsServerZone
+foreach ($DnsRecord in $DnsRecords){
+    Add-DnsServerResourceRecordA -ZoneName $guidDNSZoneName -Name $DnsRecord.vm -IPv4Address $DnsRecord.IP -computername localhost -CreatePtr   
+}
+# list created records
+Get-DnsServerResourceRecord -ZoneName $guidDNSZoneName -RRType A
